361343770593de6158176cb6bdf4e8f03f5fa2b5
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-07-15 14:28:44 -0700

    Changed to if-else from switch

--- main.py
+++ main.py
@@ -7,60 +7,59 @@ while True:
     action = input(prompt)
     action = action.strip()
 
-    match action:
-        case 'add':
-            todo = input("add a new item: ") + "\n"
+    if 'add' in action:
+        todo = action[4:]
+        
+        # Read data from todo text file
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+
+        todos.append(todo)
+        
+        # write data to todo text file
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
             
-            # Read data from todo text file
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+    elif 'show' in action:
+        # Read data from todo text file
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(todo)
-            
-            # write data to todo text file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-            
-        case 'show':
-            # Read data from todo text file
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    elif 'edit' in action:
+        # Read data from todo text file
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index + 1}-{item}"
-                print(row)
-        case 'edit':
-            # Read data from todo text file
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-            for item in todos:
-                print(item)
-            number = input("What is the number of todo to edit: ")
-            number = int(number) - 1
+        number = int(action[5:])
+        number = number - 1
+        
+        new_todo = input("Enter a new todo: ")
+        todos[number] = new_todo + '\n'
 
+        # write data to todo text file
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+    elif 'remove' in action:
+        # Read data from todo text file
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+        number = int(action[7:])
+        index = number - 1
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
 
-            new_todo = input("Enter a new todo: ")
-            todos[number] = new_todo + '\n'
+        # write data to todo text file
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+        message = f"Todo {todo_to_remove} was removed from the list."
+        print(message)
 
-            # write data to todo text file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-        case 'remove':
-            # Read data from todo text file
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
-            number = int(input("What is the number of todo to remove: "))
-            index = number - 1
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
-
-            # write data to todo text file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-            message = f"Todo {todo_to_remove} was removed from the list."
-            print(message)
-
-        case 'exit':
-            break
+    elif 'exit' in action:
+        break
+    else:
+        print("Enter a valid command")
             
